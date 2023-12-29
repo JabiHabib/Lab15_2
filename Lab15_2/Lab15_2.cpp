4f97153fbe73a601a5fe9f0a37ebbef043e880c2
JabibHabib <kubusalsa@gmail.com>
2023-12-29 14:20:02 +0200

    Добавьте файлы проекта.

---
+++ Lab15_2/Lab15_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "Ukr");
+    int c[3];
+
+    printf("Введiть три числа: ");
+    for (int i = 0; i < 3; i++) {
+        scanf_s("%d", &c[i]);
+    }
+
+    int min = c[0];
+
+    for (int i = 1; i < 3; i++) {
+        if (c[i] < min) {
+            min = c[i];
+        }
+    }
+
+    printf("Найменше число: %d\n", min);
+
+    return 0;
+}
